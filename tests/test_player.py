32c167775939f3a59b20fa2f73e8a674a033a8a5
Andrew Lindberg <alindberg@fastmail.net>
2017-05-28 17:55:07 -0400

    Added additional tests for player's shield

--- tests/test_player.py
+++ tests/test_player.py
@@ -1,5 +1,4 @@
-import pytest
-from game.models import Player
+from game.models import Player, Shield
 
 def test_player_handle_monster():
     p = Player()
@@ -8,3 +7,18 @@ def test_player_handle_monster():
     p.handle_monster(10)
     assert p.health == 6
 
+
+def test_player_handle_monster_with_shield():
+    p = Player()
+    p.shield = Shield(8)
+    p.handle_monster(10)
+    assert p.health == 19
+    assert p.shield.previous_value == 10
+
+
+def test_player_shield_breaks():
+    p = Player()
+    p.shield = Shield(3)
+    p.handle_monster(3)
+    p.handle_monster(3)
+    assert p.shield is None
